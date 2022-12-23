d25d8186d66bec684abf4b9f11f0729d75549bbf
josuepanzo <josuepanzo91@gmail.com>
2022-12-22 23:23:31 -0300

    Personal Barker Portfolio

---
+++ _js/script.js
@@ -0,0 +1,10 @@
+
+var nav_toggle = document.getElementById('nav-toggle');
+
+function showMenu() {
+    nav_toggle.style.display = "block";
+}
+
+function hiddenMenu() {
+    nav_toggle.style.display = "none";
+}
